eadc566b99b18adaef8d2bc02c19e8c2bf6e3a30
Thomat EdiXon <son95847@gmail.com>
2023-02-09 14:09:13 +0700

    register_confirm

--- src/main/java/io/aptech/Controller/RegisterController.java
+++ src/main/java/io/aptech/Controller/RegisterController.java
@@ -32,7 +32,6 @@ public class RegisterController implements Initializable {
     @FXML private Label err_password;
     @FXML private Label err_email;
     @FXML private Label err_phone;
-    private int number_error = 0;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         btn_register.setOnAction(e->{
@@ -41,54 +40,79 @@ public class RegisterController implements Initializable {
             String resPassword = res_password.getText();
             String resEmail = res_email.getText();
             String resPhone = res_phone.getText();
+            String check_resFullName = "YES";
+            String check_resUsername = "YES";
+            String check_resPassword = "YES";
+            String check_resEmail = "YES";
+            String check_resPhone = "YES";
             String passwordEncoder = BCrypt.hashpw(resPassword,BCrypt.gensalt(12));
             // check full name
             if(resFullName.length() == 0 ){
+                check_resFullName = "NO";
                 err_fullname.setText("Full name is required ");
                 err_fullname.setStyle("-fx-text-fill: #ff1744");
             } else if (RegisterValidation.checkFullName(resFullName)=="NO") {
+                check_resFullName = "NO";
                 err_fullname.setText("Full name required 6-50 characters");
                 err_fullname.setStyle("-fx-text-fill: #ff1744");
-                number_error +=1;
+            }else{
+                check_resFullName = "YES";
+                err_fullname.setText("");
             }
             //check username
             if(resUsername.length() == 0 ){
+                check_resUsername = "NO";
                 err_username.setText("Username is required ");
                 err_username.setStyle("-fx-text-fill: #ff1744");
             } else if (RegisterValidation.checkUsername(resUsername)=="NO") {
+                check_resUsername = "NO";
                 err_username.setText("Username required 6-50 characters");
                 err_username.setStyle("-fx-text-fill: #ff1744");
-                number_error +=1;
+            }else{
+                check_resUsername = "YES";
+                err_username.setText("");
             }
             //check password
             if(resPassword.length() == 0 ){
+                check_resPassword = "NO";
                 err_password.setText("Password is required ");
                 err_password.setStyle("-fx-text-fill: #ff1744");
             }else if (RegisterValidation.checkPassword(resPassword)=="NO"){
+                check_resPassword = "NO";
                 err_password.setText("Password required 6-50 include number and characters");
                 err_password.setStyle("-fx-text-fill: #ff1744");
-                number_error +=1;
+            }else{
+                check_resPassword = "YES";
+                err_password.setText("");
             }
             //check email
             if (resEmail.length()==0){
+                check_resEmail = "NO";
                 err_email.setText("Email is required");
                 err_email.setStyle("-fx-text-fill: #ff1744");
             } else if (RegisterValidation.checkEmail(resEmail)=="NO") {
+                check_resEmail = "NO";
                 err_email.setText("Email is invalid");
                 err_email.setStyle("-fx-text-fill: #ff1744");
-                number_error +=1;
+            }else {
+                check_resEmail = "YES";
+                err_email.setText("");
             }
             //check phone
             if (resPhone.length()==0){
+                check_resPhone = "YES";
                 err_phone.setText("Phone is required");
                 err_phone.setStyle("-fx-text-fill: #ff1744");
             } else if (RegisterValidation.checkPhone(resPhone)=="NO") {
+                check_resPhone = "YES";
                 err_phone.setText("Phone is invalid");
                 err_phone.setStyle("-fx-text-fill: #ff1744");
-                number_error +=1;
+            }else {
+                check_resPhone = "YES";
+                err_phone.setText("");
             }
             //check error before register and insert
-            if(number_error == 0){
+            if(check_resFullName == "YES" && check_resPassword == "YES" && check_resUsername == "YES" && check_resEmail == "YES" && check_resPhone=="YES"){
                 User n_user = new User();
                 n_user.setFullName(resFullName);
                 n_user.setUsername(resUsername);
@@ -98,9 +122,9 @@ public class RegisterController implements Initializable {
                 UserStatement ust = new UserStatement();
                 ust.insert(n_user);
                 //close window
-                    Node node = (Node) e.getSource();
-                    Stage thisStage = (Stage) node.getScene().getWindow();
-                    thisStage.close();
+                Node node = (Node) e.getSource();
+                Stage thisStage = (Stage) node.getScene().getWindow();
+                thisStage.close();
                 //load login window
                 loadLoginWindow();
             }
